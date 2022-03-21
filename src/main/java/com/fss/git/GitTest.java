5d172a04868dc25d67619ea29dd15bdaa5282eaf
Blind0525 <101966852+Blind0525@users.noreply.github.com>
2022-03-21 16:43:47 +0800

    pull commit

--- src/main/java/com/fss/git/GitTest.java
+++ src/main/java/com/fss/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("master冲突");
         System.out.println("hot-fix冲突");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
